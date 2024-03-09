f0312d443c4c428a43519b954eb3b4ea2622932f
Anukul0317 <anukul0317@gmail.com>
2024-03-09 16:36:03 +0530

    add a responsive mobile navigation menu

---
+++ src/utils/mobile-nav.js
@@ -0,0 +1,29 @@
+const mobileNav = () => {
+  const headerBtn = document.querySelector(".header__bars");
+  const mobileNav = document.querySelector(".mobile-nav");
+  const mobileNavLinks = document.querySelectorAll(".mobile-nav__link");
+
+  // state
+  let isMobileNavOpen = true;
+
+  headerBtn.addEventListener("click", () => {
+    isMobileNavOpen = !isMobileNavOpen;
+    if (isMobileNavOpen) {
+      mobileNav.style.display = "flex";
+      document.body.style.overflowY = "hidden";
+    } else {
+      mobileNav.style.display = "none";
+      document.body.style.overflowY = "auto";
+    }
+    console.log(isMobileNavOpen);
+  });
+  mobileNavLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      isMobileNavOpen = false;
+      mobileNav.style.display = "none";
+      document.body.style.overflowY = "auto";
+    });
+  });
+};
+
+export default mobileNav;
